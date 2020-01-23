ddd1429ca383fe59ad6b9ad55df5cb76e43f27da
knoc5215@naver.com <dbgmd256>
2020-01-23 10:07:10 +0900

    leetcode

--- src/leetcode/Array/TowSum2_Input_array_is_sorted.java
+++ src/leetcode/Array/TowSum2_Input_array_is_sorted.java
@@ -1,52 +1,54 @@
 package leetcode.Array;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class TowSum2_Input_array_is_sorted {
     public static void main(String[] args) {
-//         int[] ret = twoSum(new int[]{2, 7, 11, 15}, 9);
+        int[] ret = twoSum(new int[]{2, 7, 11, 15}, 9);
 //        int[] ret = twoSum(new int[]{0, 0, 3, 4}, 0);
-        int[] ret = twoSum(new int[]{2, 3, 4}, 6);
+//        int[] ret = twoSum(new int[]{2, 3, 4}, 6);
+//        int[] ret = twoSum(new int[]{-1, 0}, -1);
 
     }
 
     public static int[] twoSum(int[] numbers, int target) {
-        Arrays.sort(numbers);
 
-        int[] indexArray = new int[2];
-        for (int i = 0; i < numbers.length - 1; i++) {
-
-            int a = numbers[i];
-            int b = numbers[i + 1];
-            System.out.println("a is " + numbers[i]);
-            System.out.println("b is " + numbers[i + 1]);
-
-            if (b < target && (i + 1) == numbers.length - 1) {
-                indexArray[0] = i;
-                indexArray[1] = i + 1 + 1;
-                for (int val : indexArray) {
-                    System.out.print(val + " ");
-                }
-                return indexArray;
-            }
-
-            if (a <= target && target <= b) {
-                System.out.println("\t find");
-                indexArray[0] = i;
-                indexArray[1] = (i + 1);
+        Map<Integer, Integer> map = new HashMap<>();
 
 
-                for (int val : indexArray) {
-                    System.out.print(val + " ");
-                }
+        int[] ret = new int[2];
 
-                return indexArray;
+        for (int i = 0; i < numbers.length; i++) {
+            int complete = target - numbers[i];
+            System.out.println("complete : " + complete);
+
+            if (map.containsKey(complete)) {
+                ret[0] = i + 1;
+                ret[1] = map.get(complete) + 1;
+                break;
+            } else {
+                map.put(numbers[i], i);
 
             }
-
         }
-        return null;
 
+        List<Integer> list = new ArrayList<>();
+        for (int val : ret) {
+//          System.out.print(val + " ");
+            list.add(val);
+        }
 
+        Collections.reverse(list);
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+            System.out.print(result[i] + " ");
+        }
+        System.out.println();
+
+        return result;
     }
+
 }
