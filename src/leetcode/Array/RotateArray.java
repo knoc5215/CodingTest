342f5393ac51a192790f9a39dc3e9f146b45072d
knoc5215@naver.com <dbgmd256>
2020-03-30 11:01:58 +0900

    SWEA START

---
+++ src/leetcode/Array/RotateArray.java
@@ -0,0 +1,35 @@
+package leetcode.Array;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class RotateArray {
+
+    public static void rotate(int[] nums, int k) {
+
+
+        for (int step = k; step > 0; step--) {
+            for (int i = 0; i < nums.length - 1; i++) {
+                int temp = nums[i + 1];
+                nums[i + 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        for (int val : nums) {
+            System.out.print(val + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};    // 5 6 7 1 2 3 4
+        int step = 3;
+
+        rotate(arr, step);
+
+
+    }
+}
