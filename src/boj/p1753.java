cac7a1ac6b7b123cf10e788c5b4aa03d0a2db90b
knoc5215 <knoc5215@naver.com>
2020-09-14 21:57:58 +0900

    백준 다익스트라 최단경로

--- src/boj/p1753.java
+++ src/boj/p1753.java
@@ -1,90 +1,88 @@
 package boj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
+/*
+    백준 1753번 최단거리 문제
+    다익스트라
+    인접리스트 (2차원 배열로 풀면 메모리 초과)
+ */
 public class p1753 {
-    static int[][] ad;
-    static boolean[] visit;
-    static int[] dist;
     static int V, E;
-    static int start;
-    static int MAX_V = 20000;
-    static final int INF = 1000000000;
-    static List<List<Pair>> P;
+    static int K;
+    static List<Node>[] lists;
+    public static final int INF = Integer.MAX_VALUE;
 
-    static PriorityQueue<Pair> pq = new PriorityQueue<>(new Comparator<Pair>() {
-        @Override
-        public int compare(Pair o1, Pair o2) {
-            return o1.dist - o2.dist;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        V = Integer.parseInt(st.nextToken());   // 정점의 개수
+        E = Integer.parseInt(st.nextToken());   // 간선의 개수
+
+        st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());   // 시작점
+
+        lists = new ArrayList[V + 1];   // 인접리스트 1~V
+        for (int i = 1; i < V + 1; i++) {
+            lists[i] = new ArrayList<>();
         }
-    });
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        V = sc.nextInt();   // 정점의 개수
-        E = sc.nextInt();   // 간선의 개수
-        start = sc.nextInt() - 1;
+        int[] dist = new int[V + 1];
+        Arrays.fill(dist, INF); // 모든 정점으로 거리 무한대로 초기화
 
-        P = new ArrayList<>();
-        for (int i = 0; i < MAX_V; i++) {
-            P.add(new ArrayList<>());
-        }
 
         for (int i = 0; i < E; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            int w = sc.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
 
-            P.get(x - 1).add(new Pair(y - 1, w));
+            lists[from].add(new Node(to, cost));
+
         }
 
-        dist = new int[MAX_V];
-        Arrays.fill(dist, INF);
-        visit = new boolean[MAX_V];
 
-        dist[start] = 0;
-        pq.add(new Pair(start, 0));
-        while (!pq.isEmpty()) {
-            int cur;
-            do {
-                cur = pq.peek().pos;
-                pq.poll();
-            } while (!pq.isEmpty() && visit[cur]);
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost); // 아직 방문하지 않은 정점들 중 거리가 가장 가까운 정점을 하나 선택하기 위한 우선순위 큐
+        priorityQueue.add(new Node(K, 0));
+        dist[K] = 0;    // 시작점  거리 0으로 초기화
 
-            if (visit[cur]) {
-                break;
-            }
+        boolean[] visit = new boolean[V + 1];
 
+        while (!priorityQueue.isEmpty()) {
+            Node curNode = priorityQueue.poll();    // 가장 가까운 정점이 나온다
+            int cur = curNode.vertex;
+
+            if (visit[cur]) continue;   // 아직 방문하지 않았을때만 진행
             visit[cur] = true;
-            List<Pair> curList = P.get(cur);
-            for (Pair p : curList) {
-                int next = p.pos;
-                int d = p.dist;
 
-                if (dist[next] > dist[cur] + d) {
-                    dist[next] = dist[cur] + d;
-                    pq.add(new Pair(next, dist[next]));
+            for (Node node : lists[cur]) {  // 해당 정점의 인접한 정점들에 대해서
+                if (dist[node.vertex] > dist[cur] + node.cost) {    // 더 짧은 경로가 있다면
+                    dist[node.vertex] = dist[cur] + node.cost;  // 갱신해주고
+                    priorityQueue.add(new Node(node.vertex, dist[node.vertex]));
                 }
             }
 
+
         }
 
-        for (int i = 0; i < V; i++) {
-            if (dist[i] == INF) {
-                System.out.println("INF");
-            } else {
-                System.out.println(dist[i]);
-            }
+        for (int i = 1; i < dist.length; i++) {
+            int cost = dist[i];
+            System.out.println(cost == INF ? "INF" : cost);
+        }
+
+    }
+
+    static class Node {
+        int vertex, cost;
+
+        public Node(int vertex, int cost) {
+            this.vertex = vertex;
+            this.cost = cost;
         }
     }
 
 
-    static class Pair {
-        int pos, dist;
-
-        public Pair(int pos, int dist) {
-            this.pos = pos;
-            this.dist = dist;
-        }
-    }
 }
