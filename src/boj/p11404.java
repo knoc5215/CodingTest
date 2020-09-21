fe4fecda5c05dcb341187f15076c11683593b60e
knoc5215 <knoc5215@naver.com>
2020-09-21 21:23:24 +0900

    백준 플로이드-와샬 플로이드

--- src/boj/p11404.java
+++ src/boj/p11404.java
@@ -13,20 +13,20 @@ O(V^3)
 public class p11404 {
     static int n, m;
     static int[][] dist;
-    public static final int INF = 1000000;
+    public static final int INF = 1000000000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
-        dist = new int[n + 1][n + 1];
+        dist = new int[100][100];
 
         st = new StringTokenizer(br.readLine());
         m = Integer.parseInt(st.nextToken());
 
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 if (i == j) {
                     dist[i][j] = 0;
                 } else {
@@ -37,25 +37,26 @@ public class p11404 {
 
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken()) - 1;
             int cost = Integer.parseInt(st.nextToken());
 
 
             dist[from][to] = Math.min(cost, dist[from][to]);
         }
 
-        for (int k = 1; k < n + 1; k++) {   // k번째 정점까지만 사용했을때
-            for (int i = 1; i < n + 1; i++) {
-                for (int j = 1; j < n + 1; j++) {
+        for (int k = 0; k < n; k++) {   // k번째 정점까지만 사용했을때
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
                     dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]); // k번째 정점을 우회해서 가는 최단경로가 있는지
                 }
             }
         }
 
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                System.out.print(dist[i][j] + " ");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int output = dist[i][j] >= INF ? 0 : dist[i][j];
+                System.out.print(output + " ");
             }
             System.out.println();
         }
