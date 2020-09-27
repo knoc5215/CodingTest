37f1bc45e7236e03b3052a20788c06e8ba2cc670
knoc5215 <knoc5215@naver.com>
2020-09-27 16:58:36 +0900

    백준 구현 별찍기8

---
+++ src/boj/p2445.java
@@ -0,0 +1,42 @@
+package boj;
+
+import java.util.Scanner;
+
+public class p2445 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= 2 * n; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= 2 * i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
