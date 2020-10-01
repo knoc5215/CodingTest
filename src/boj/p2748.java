10e1f3f1d752f3e8c403d2c51fe9cd85f439aee8
knoc5215 <knoc5215@naver.com>
2020-10-01 14:26:22 +0900

    백준 DP 피보나치 수2

---
+++ src/boj/p2748.java
@@ -0,0 +1,35 @@
+package boj;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class p2748 {
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+
+        List<Long> fibList = new ArrayList<>(); // 범위 주의 long으로 해야함
+        fibList.add((long) 0);
+        fibList.add((long) 1);
+        if (n >= 2) {   // 피보나치 동작 조건
+            while (true) {
+                int size = fibList.size();
+                long next = fibList.get(size - 2) + fibList.get(size - 1);
+                fibList.add(next);
+
+                if (fibList.size() == n + 1)
+                    break;
+            }
+            System.out.println(fibList.get(fibList.size() - 1));
+        } else {
+            System.out.println(n);
+        }
+
+
+    }
+
+
+}
