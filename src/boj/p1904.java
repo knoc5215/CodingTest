4918175faedd881bdf170e04868fa7a7e6606df5
knoc5215 <knoc5215@naver.com>
2020-10-03 18:04:30 +0900

    백준 DP 01타일

---
+++ src/boj/p1904.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.util.Scanner;
+
+public class p1904 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[1000001];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i < 1000001; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 15746;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
