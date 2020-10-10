9b9df0081d6afd824717f50be749d2daf19034b8
knoc5215 <knoc5215@naver.com>
2020-10-10 14:44:06 +0900

    백준 그리디 단어 수학

--- src/boj/p1339.java
+++ src/boj/p1339.java
@@ -6,13 +6,13 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class p1339 {
-    static Map<Character, Integer> map;
-    static Set<Character> set;
-    static List<String> words;
-    static List<Character> alphabet;
-    static boolean[] visit;
+    static Map<Character, Integer> map; // 문자 - 값
+    static Set<Character> set;  // 알파벳 중복제거
+    static List<String> words;  // 단어 리스트
+    static List<Character> alphabet;    // 알파벳 중복제거된 리스트
+    static boolean[] visit; // 방문체크
     static int n;
-    static int max = Integer.MIN_VALUE;
+    static int max = Integer.MIN_VALUE; // 최대값 계산하기
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,29 +28,29 @@ public class p1339 {
             words.add(word);
             char[] chars = word.toCharArray();
             for (char ch : chars) {
-                set.add(ch);
+                set.add(ch);    // 알파벳 중복제거
             }
         }
         alphabet = new ArrayList<>(set);
-        permutation(0);
+        permutation(0); // 알파벳 순열 만들기
         System.out.println(max);
 
     }
 
     static void permutation(int depth) {
-        if (depth == alphabet.size()) {
+        if (depth == alphabet.size()) {     // 모든 알파벳 셋팅이 끝났다면
             int sum = 0;
             for (String word : words) {
-                sum += wordSum(word);
+                sum += wordSum(word);   // 단어마다 숫자 누적합
             }
-            max = Math.max(max, sum);
+            max = Math.max(max, sum);   // 최대값 갱신
             return;
         }
 
         for (int i = 0; i < 10; i++) {
             if (!visit[i]) {
                 visit[i] = true;
-                map.put(alphabet.get(depth), i);
+                map.put(alphabet.get(depth), i);    // 알파벳 순열만들면서, 값도 갱신
                 permutation(depth + 1);
                 visit[i] = false;
             }
@@ -58,11 +58,11 @@ public class p1339 {
     }
 
 
-    static int wordSum(String word) {
+    static int wordSum(String word) {   // string으로하면 부담스러움
         int sum = 0;
         for (int i = 0; i < word.length(); i++) {
             char ch = word.charAt(i);
-            sum *= 10;
+            sum *= 10;  // 자릿수 고려해서
             sum += map.get(ch);
         }
         return sum;
