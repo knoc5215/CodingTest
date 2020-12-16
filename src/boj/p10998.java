dc624d25d7edd5372e87b9bb7b6d40a280c7e74d
knoc5215 <knoc5215@naver.com>
2020-12-16 21:50:39 +0900

    백준 CLASS_1 [A*B]

---
+++ src/boj/p10998.java
@@ -0,0 +1,11 @@
+package boj;
+
+import java.util.Scanner;
+
+public class p10998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() * sc.nextInt());
+    }
+
+}
