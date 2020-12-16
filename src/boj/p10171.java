29c185fabf43c0cab885866692b75f7f612908c3
knoc5215 <knoc5215@naver.com>
2020-12-16 21:56:54 +0900

    백준 CLASS_1 [고양이]

---
+++ src/boj/p10171.java
@@ -0,0 +1,10 @@
+package boj;
+
+public class p10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
