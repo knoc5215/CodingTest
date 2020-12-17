8f7158484c559377a6d9f2e7751ea17662b0ced2
knoc5215 <knoc5215@naver.com>
2020-12-17 22:16:27 +0900

    백준 CLASS_2 [수 찾기]

---
+++ src/boj/p1920.java
@@ -0,0 +1,28 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class p1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        Set<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            System.out.println(set.contains(Integer.parseInt(st.nextToken())) ? 1 : 0);
+
+        }
+    }
+}
