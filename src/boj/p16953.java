ac6e27d3c58db39ae258d8fd59f6633512e4447f
knoc5215 <knoc5215@naver.com>
2021-01-17 21:58:30 +0900

    백준 CLASS_4 [A->B]

---
+++ src/boj/p16953.java
@@ -0,0 +1,33 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class p16953 {
+    public static long a, b;
+    public static boolean isDone = false;
+    public static int ret = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        a = Long.parseLong(st.nextToken());
+        b = Long.parseLong(st.nextToken());
+
+        solve(a, 0);
+
+        System.out.println(isDone ? ret + 1 : -1);
+    }
+
+    static void solve(long from, int count) {
+        if (from == b) {
+            isDone = true;
+            ret = count;
+        } else if (from < b) {
+            solve(from * 2, count + 1);
+            solve(Long.parseLong(String.valueOf(from) + "1"), count + 1);
+        }
+    }
+}
