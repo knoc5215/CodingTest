b3b82e22188e24107bbbe84ea2438fd9e2567d48
knoc5215 <knoc5215@naver.com>
2021-02-13 17:54:02 +0900

    N과 M (6)

---
+++ src/boj/p15655.java
@@ -0,0 +1,52 @@
+package boj;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p15655 {
+    static int N, M;
+    static int[] arr, out;
+    static boolean[] visit;
+    static StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[N];
+        visit = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        out = new int[M];
+        DFS(0, 0);
+
+        System.out.println(stringBuilder);
+
+
+    }
+
+    private static void DFS(int start, int depth) {
+        if (depth == M) {
+            for (int val : out) {
+                stringBuilder.append(val).append(' ');
+            }
+            stringBuilder.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                out[depth] = arr[i];
+                DFS(i + 1, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+
+}
