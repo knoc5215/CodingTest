cdd981a0ac97e37e32dc72f4ee9baaca1aa70a4b
knoc5215 <knoc5215@naver.com>
2021-02-21 15:31:43 +0900

    팩토리얼 0의 개수

---
+++ src/boj/p1676.java
@@ -0,0 +1,22 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class p1676 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int count = 0;
+
+        while (num >= 5) {
+            count += num / 5;
+            num /= 5;
+        }
+        System.out.println(count);
+    }
+
+
+}
