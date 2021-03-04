447e78596227886eb259dde808c188d6ec169e29
knoc5215 <knoc5215@naver.com>
2021-03-04 22:20:42 +0900

    수 정렬하기2(병합)

--- src/boj/p2751.java
+++ src/boj/p2751.java
@@ -1,38 +1,71 @@
 package boj;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
 
 public class p2751 {
-    static int n;
-    static ArrayList<Integer> list;
+    static int[] temp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = stoi(br.readLine());
-        list = new ArrayList<>();
 
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n]; // 5 2 3 4 1
+        temp = new int[arr.length];
         for (int i = 0; i < n; i++) {
-            list.add(stoi(br.readLine()));
-        }
-        br.close();
-
-        Collections.sort(list);
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int val : list) {
-            stringBuilder.append(val).append('\n');
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-        System.out.println(stringBuilder);
-
-
+        merge_sort(arr, 0, arr.length - 1);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int val : arr) {
+            bw.write(String.valueOf(val));
+            bw.newLine();
+        }
+        bw.flush();
     }
 
-    public static int stoi(String s) {
-        return Integer.parseInt(s);
+    private static void merge_sort(int[] arr, int start, int end) {
+        if (start < end) {
+            int mid = (start + end) / 2;
+            merge_sort(arr, start, mid);
+            merge_sort(arr, mid + 1, end);
+
+            merge(arr, start, mid, end);
+        }
     }
+
+    private static void merge(int[] arr, int start, int mid, int end) {
+        int i = start;
+        int j = mid + 1;
+
+        int k = start;
+
+        while (i <= mid && j <= end) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= end) {
+            temp[k++] = arr[j++];
+        }
+
+        if (end + 1 - start >= 0) System.arraycopy(temp, start, arr, start, end + 1 - start);
+    }
+
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
 }
